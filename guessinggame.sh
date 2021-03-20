02525dbee618494e01a9fe689b56c49a01dc1906
phanendhar <phanendharreddy.7@gmail.com>
2021-03-20 21:34:58 +0530

    Updated main script

---
+++ guessinggame.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "please guess the number of files in the directory: "
+read num
+value=`ls|wc -l`
+if [[$num -eq $value]]
+then
+echo "Congratulations"
+else
+echo "Try again"
+fi
+
+
