a130fc676cacc2ab61b5d2d545acfc533719df09
phanendhar <phanendharreddy.7@gmail.com>
2021-03-20 22:20:59 +0530

    Completed script of gussing game

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
-
-echo "please guess the number of files in the directory: "
-read num
+echo "How many files are in the directory"
 value=`ls|wc -l`
+out=0
+function game {
+while [[ $out -eq 0 ]]
+do
+echo "Just guess the number of files in the directory: "
+read num
 if [[ $num -eq $value ]]
 then
-echo "Congratulations"
+out=1
+echo "congratulations you have guessed it right!!!"
+elif [[ $num -lt $value ]] 
+then
+echo "Your guess is too low"
+out=0
 else
-echo "Try again"
+echo "your guess is too high"
+out=0
 fi
+done
+}
+game
 
 
